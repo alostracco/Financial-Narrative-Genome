52f5ab84ee4fc3b741fb218cab0dabb560d79ccc
sudrishas <sudrishasarkar005@gmail.com>
2024-11-22 15:27:33 -0500

    last changes to frontend-1 branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import './index.css';
-import Sentiment from './sentiment.js';  // Update to use the capitalized Sentiment component
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
 
 function App() {
   return (
-    <div className="App">
-      <Sentiment />  {/* Use the capitalized Sentiment component */}
+    <div>
+      <Navbar />
+      <Hero />
     </div>
   );
 }
 
 export default App;
-
